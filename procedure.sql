4158f6db30a786d21d7afbc142af3e782f139518
jpedrocrvg <137358244+jpedrocrvg@users.noreply.github.com>
2023-11-09 17:51:14 -0300

    procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,22 +1,18 @@
 CREATE OR REPLACE PROCEDURE atualizar_informacoes_cliente(
     IN p_id_cliente INT,
+    IN p_id_agencia INT,
     IN p_primeiro_nome VARCHAR(255),
     IN p_sobrenome VARCHAR(255),
-    IN p_telefone INT,
-    IN p_cpf INT,
-    IN p_ident INT
+    IN p_telefone INT
 )
 LANGUAGE plpgsql AS $$
 BEGIN
     UPDATE CLIENTES
     SET 
+        id_agencia = p_id_agencia,
         primeiro_nome = p_primeiro_nome,
         sobrenome = p_sobrenome,
-        telefone = p_telefone,
-        cpf = p_cpf,
-        ident = p_ident
+        telefone = p_telefone
     WHERE id_cliente = p_id_cliente;
-
-    RAISE NOTICE 'Informações do cliente % atualizadas com sucesso.', p_id_cliente;
 END;
 $$;
