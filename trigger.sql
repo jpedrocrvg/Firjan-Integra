8ee5dfddd19aa26884e2c8b7c8e43109ac49c2a0
jpedrocrvg <137358244+jpedrocrvg@users.noreply.github.com>
2023-11-09 12:43:16 -0300

    Add files via upload

---
+++ trigger.sql
@@ -0,0 +1,19 @@
+ALTER TABLE CLIENTES
+ADD COLUMN saldo NUMERIC DEFAULT 0;
+
+CREATE OR REPLACE FUNCTION atualiza_saldo_cliente()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE CLIENTES
+    SET saldo = saldo - NEW.valor::numeric
+    WHERE id_cliente = NEW.id_cliente;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_atualiza_saldo_cliente
+AFTER INSERT ON SAQUE
+FOR EACH ROW
+EXECUTE FUNCTION atualiza_saldo_cliente();
+
